d39ac53d05d168c9bd0f8eaa081f7180a0dd87a3
thetansabe <thetannguyen193@gmail.com>
2023-06-01 00:06:08 +0700

    configureDbContext.clients, working with ids4 db tables

---
+++ ids/Quickstart/Client/ClientController.cs
@@ -0,0 +1,58 @@
+﻿using IdentityServer4.Events;
+using IdentityServer4.Models;
+using IdentityServer4.Services;
+using IdentityServer4.Extensions;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Linq;
+using System.Threading.Tasks;
+using IdentityServer4.Validation;
+using System.Collections.Generic;
+using System;
+using IdentityServer4.EntityFramework.DbContexts;
+using Microsoft.EntityFrameworkCore;
+using IdentityServer4.EntityFramework.Entities;
+using System.Text.Json.Serialization;
+using System.Text.Json;
+
+namespace IdentityServerHost.Quickstart.UI
+{
+    public class ClientController : Controller
+    {
+        private readonly ConfigurationDbContext _configDbContext;
+        private readonly PersistedGrantDbContext _persistedGrantDbContext;
+
+        public ClientController(ConfigurationDbContext configDbContext, PersistedGrantDbContext persistedGrantDbContext)
+        {
+            _configDbContext = configDbContext;
+            _persistedGrantDbContext = persistedGrantDbContext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var clients = await _configDbContext.Clients
+                                .Include(c => c.RedirectUris)
+                                .ToListAsync();
+
+            var jsonOptions = new JsonSerializerOptions
+            {
+                ReferenceHandler = ReferenceHandler.Preserve,
+                IgnoreNullValues = true,
+                Converters =
+                    {
+                        new JsonStringEnumConverter()
+                    }
+            };
+
+            var json = JsonSerializer.Serialize(clients, jsonOptions);
+
+            var viewModels = JsonSerializer.Deserialize<List<ClientViewModel>>(json, jsonOptions);
+
+            //return Ok(json);
+            return View("Client", viewModels);
+        }
+
+    }
+}
---
+++ ids/Quickstart/Client/ClientViewModel.cs
@@ -0,0 +1,25 @@
+﻿namespace IdentityServerHost.Quickstart.UI
+{
+    public class ClientViewModel
+    {
+        public int Id { get; set; }
+        public bool Enabled { get; set; }
+        public string ClientId { get; set; }
+        public string? ClientName { get; set; }
+        //public string ProtocolType { get; set; }
+        //public bool RequireClientSecret { get; set; }
+        //public bool RequireConsent { get; set; }
+        //public bool AllowRememberConsent { get; set; }
+        //public bool AlwaysIncludeUserClaimsInIdToken { get; set; }
+        //public bool RequirePkce { get; set; }
+        //public bool AllowPlainTextPkce { get; set; }
+        //public bool RequireRequestObject { get; set; }
+        //public bool AllowAccessTokensViaBrowser { get; set; }
+        public List<RedirectUriViewModel> RedirectUris { get; set; }
+    }
+
+    public class RedirectUriViewModel
+    {
+        public string RedirectUri { get; set; }
+    }
+}
